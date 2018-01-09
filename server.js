9008d0079d18772d36e56f382df208b56b685287
Tabish Raza <tabishraza@gmail.com>
2018-01-09 13:57:12 +0000

    port config

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/data/db')
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log('test');
+});
 
 app.get('/', function(request, response){
   response.send("Hello world")
