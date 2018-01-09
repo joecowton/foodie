9c1fa570884965a6a7a847f62faedbacba52c88f
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-09 18:12:15 +0000

    Added dependencies

--- routes/product.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var router = express.Router();
-
-// Load product model:
-require('../models/Product');
-var Product = mongoose.model('products');
-
-// ____ ROUTES : ____
