c08946e4e978811979c46e5f9deed8b1fb7e0c31
Tabish Raza <tabishraza@gmail.com>
2018-01-09 18:18:46 +0000

    user added to database

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: 'auth/google/callback'
+      callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleID: profile.id })
