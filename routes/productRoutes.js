8161691aaa86bf5d11bd9b400d7e7f49adb57b1f
Joe Cowton <joecowton@gmail.com>
2018-01-11 12:37:05 +0000

    routes for preference

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,18 +1,7 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('products');
-const User = mongoose.model('users');
 
 module.exports = app => {
-  // app.get('/products', function(req, res){
-  //   Product.find({})
-  //     .sort({expiryDate: 'desc'})
-  //     .then(function(products) {
-  //       res.render('products/list', {
-  //         products: products
-  //       });
-  //   });
-  // });
-
   app.get('/api/products', function(req, res){
     Product.find({})
       .sort({expiryDate: 'desc'})
@@ -30,15 +19,6 @@ module.exports = app => {
     });
   });
 
-  app.get('/api/user/:name', function(req, res){
-    User.find({ name: req.params.name })
-      .then(function(user) {
-        res.json(user);
-    });
-  });
-
-
-
 //sends delete request remember not to test in google
   app.delete('/api/products/delete/:title', function(req, res){
     Product.remove({ title: req.params.title})
@@ -46,12 +26,4 @@ module.exports = app => {
         console.log("deleted");
     });
   });
-
-  // app.get('/', function(req, res){
-  // 	Product.find({}, function(err, docs){
-  // 		if(err) res.json(err);
-  // 		else    res.render('index', {products: docs});
-  // 	});
-  // });
-
 }
---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+module.exports = app => {
+  // search by name
+  app.get('/api/user/name/:name', function(req, res){
+    User.find({ name: req.params.name })
+      .then(function(user) {
+        res.json(user);
+    });
+  });
+
+  app.get('/api/user/preference/:preference', function(req, res){
+    User.find({ preference: req.params.preference })
+      .then(function(user) {
+        res.json(user);
+    });
+  });
+}
