ff07ac0b1e5e85b5eb169700de705f972b2b5821
Tabish Raza <tabishraza@gmail.com>
2018-01-13 18:29:36 +0000

    adding product id to db

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
+require ('../models/User')
 const User = mongoose.model('users');
 const Product = mongoose.model('products');
 
-
+// let test = new User();
 
 module.exports = app => {
   app.post('/api/addwishlist', function(req, res){
-    console.log(req.body.name);
+
+    console.log("BEFORE wishlist",req.user.wishlist);
+    console.log("USER",req.user);
+    console.log("PRODUCT ID",req.body.id);
+
+    req.user.product_id.push(`${req.body.id}`);
+    // user.wishlist.push(req.user)
+    console.log("after wishlist",req.user.product_id);
   });
 }
