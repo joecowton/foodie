895dee0dcaaf339e62a1abdebf52ffe362efe308
Tabish Raza <tabishraza@gmail.com>
2018-01-13 18:28:03 +0000

    line structure

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 
 require('./models/Product')
