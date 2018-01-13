2819923b61e172bce39b299e8fd02e5996d294d9
Tabish Raza <tabishraza@gmail.com>
2018-01-13 19:50:58 +0000

    user can have multiple favorites

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -3,17 +3,9 @@ require ('../models/User')
 const User = mongoose.model('users');
 const Product = mongoose.model('products');
 
-// let test = new User();
-
 module.exports = app => {
   app.post('/api/addwishlist', function(req, res){
-
-    console.log("BEFORE wishlist",req.user.wishlist);
-    console.log("USER",req.user);
-    console.log("PRODUCT ID",req.body.id);
-
-    req.user.product_id.push(`${req.body.id}`);
-    // user.wishlist.push(req.user)
-    console.log("after wishlist",req.user.product_id);
+    req.user.product_id.push(req.body.id);
+    req.user.save();
   });
 }
