059f73dd408977204c49c14586dd215be5593215
Tabish Raza <tabishraza@gmail.com>
2018-01-13 13:13:18 +0000

    wishlist route handler

---
+++ routes/wishlistRoutes.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const Product = mongoose.model('products');
+
+
+
+// module.exports = app => {
+//   app.post('/api/addwishlist', function(req, res){
+//     console.log(req.body);
+//   });
+// }
