4deb8a43ef5d748867db2fc4debbbcb9fcfbf45b
cristhiandas <cristhiandf94@gmail.com>
2018-01-13 19:12:58 +0000

    added routers

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom'
 
 import './index.css';
 import App from './App';
@@ -12,6 +13,10 @@ import reducers from './reducers';
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </Provider>,
    document.getElementById('root'));
 registerServiceWorker();
