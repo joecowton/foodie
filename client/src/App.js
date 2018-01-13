cf24a200ffe622c65a4d1fa0d847c4437ac3137b
cristhiandas <cristhiandf94@gmail.com>
2018-01-13 18:24:13 +0000

    deleted unnecesary const

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,6 @@ import ToggleDisplay from 'react-toggle-display';
 import { connect } from 'react-redux';
 import * as actions from './actions';
 
-
-const Header = () => <h2>Header</h2>
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
-
-
 class App extends Component {
 
 constructor(props){
@@ -66,18 +59,7 @@ constructor(props){
 
       return (
         <div className="App">
-          <div className ="container">
-            <BrowserRouter>
-              <div>
-                <Header />
-                <Route exact={true} path="/" component={Landing} />
-                <Route exact={true} path="/surveys" component={Dashboard} />
-                <Route path="/surveys/new" component={SurveyNew} />
-              </div>
-            </BrowserRouter>
-          </div>
-
-
+      
           <p>Hello</p>
           {navBar}
           {searchFilter}
