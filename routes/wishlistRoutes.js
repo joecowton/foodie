1eb23acbf4b0355df3311604df84beb49d560c04
Tabish Raza <tabishraza@gmail.com>
2018-01-16 17:52:28 +0000

    delete api from backend

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -6,4 +6,9 @@ module.exports = app => {
     req.user.save();
     console.log(req.user);
   });
+
+  app.post('/api/deleteitem', function(req, res){
+    req.user.product_id.remove(req.body.id);
+    req.user.save();
+  });
 }
