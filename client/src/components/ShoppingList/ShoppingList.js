6068f89a68f3d58e5ec42aeb52cff02f632dd8c5
Tabish Raza <tabishraza@gmail.com>
2018-01-16 16:02:15 +0000

    renders users shopping list

---
+++ client/src/components/ShoppingList/ShoppingList.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Products from '../Products/Products';
+import { connect } from 'react-redux';
+
+
+class ShoppingList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      products: null,
+      list: []
+    }
+    this.favorites = this.favorites.bind(this)
+  }
+
+  componentDidMount(){
+    fetch('/api/products')
+      .then(data => data.json())
+      .then(products => {
+        this.setState({products})
+      });
+    }
+
+  favorites = () => {
+    const list = [];
+    return (
+      this.props.auth.product_id.forEach( id => {
+      this.state.products.forEach( product => {
+        if(product.id === id) {
+          this.state.list.push(product);
+        }
+      })
+    })
+    )
+  }
+
+  render(){
+    const products = <Products products={this.state.list} />
+    if(this.state.products && this.props.auth){
+      return (
+        <div>
+        {this.favorites()}
+        {products}
+        </div>
+      )
+    } return <h3>Loading products...</h3>
+    }
+}
+
+function mapStateToProps({auth}) {
+  return { auth }
+}
+
+
+export default connect(mapStateToProps) (ShoppingList);
