2aa780dfec91f11967f09b97c5d371eaffa0a55e
Cristhian Da Silva <31048842+cristhiandas@users.noreply.github.com>
2018-01-31 13:28:35 +0000

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-googleClientID: '400881132975-pjkv7no6sme3iejm6thlka1qotti0t18.apps.googleusercontent.com',
-googleClientSecret: 'BUHZFk2LTiGI4jnev2i5m69Y',
-mongoURI: 'mongodb://foodie:foodie@ds247317.mlab.com:47317/foodie-dev',
-cookieKey: 'klasdjflkjsdalkjfdskl'
-};
